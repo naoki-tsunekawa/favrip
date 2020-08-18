f2009fd9bc3b95f21f3d612110cf092257fe3466
naoki-tsunekawa <b1014138@gmail.com>
2020-08-18 23:11:50 +0900

    addEventListenerのエラー解消

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -86,28 +86,31 @@ function codeAddress(){
   });
 }
 
-window.onload = function() {
-	// modal
-	const btn = document.getElementById('btn-modal');
-	const modal = document.getElementById('modal');
-	const closeBtn = document.getElementById('closeBtn');
-	// 表示ボタン押下時処理
-	btn.addEventListener('click', function() {
-		modal.style.display = 'block';
-	});
+// addEventListenerのエラー対策
+// turbolinks有効時のwindow.loadの代わりに使う
+document.addEventListener('turbolinks:load', function() {
+		// modal
+		const btn = document.getElementById('btn-modal');
+		const modal = document.getElementById('modal');
+		const closeBtn = document.getElementById('closeBtn');
+		// 表示ボタン押下時処理
+		btn.addEventListener('click', function() {
+			modal.style.display = 'block';
+		});
 
-	// 閉じるボタン押下処理
-	closeBtn.addEventListener('click', function() {
-		modal.style.display = 'none';
-	});
-}
+		// 閉じるボタン押下処理
+		closeBtn.addEventListener('click', function() {
+			modal.style.display = 'none';
+		});
+});
+
 
 
 
 // google mapを表示
 // function dispMap(e) {
 
-// 	const status = this.el.getAttribute('data-map-status');
+// const status = this.el.getAttribute('data-map-status');
 
 // 	if (status === 'unactive') {
 // 		this.el.setAttribute('data-map-status', 'active');
