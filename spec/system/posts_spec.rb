e528c4ea82009c030924c4cd9b637972f51b4753
naoki-tsunekawa <b1014138@gmail.com>
2020-10-05 22:01:57 +0900

    update system spec

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -26,5 +26,23 @@ describe '投稿管理機能', type: :system, js: true do
 			end
 
 		end
+
+		context 'ユーザBがログインしている時' do
+			before do
+				# ユーザBを作成
+				FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+				# ユーザBでログインする
+				visit login_path
+				fill_in 'メールアドレス', with: 'b@example.com'
+				fill_in 'パスワード', with: 'password'
+				click_button 'ログイン'
+			end
+
+			it 'ユーザAが作成したタスクが表示されない' do
+				#ユーザAが作成したタスクの名称が画面上に表示されていないことを確認
+				expect(page).to have_content '最初のタスク'
+			end
+		end
+
 	end
 end
